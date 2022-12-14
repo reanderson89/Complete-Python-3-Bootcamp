8ace72f087af7809d401d0efb8fc0f1f302ace99
robAnderson <reanderson89@gmail.com>
2022-12-13 16:49:29 -0800

    completed first part of course

---
+++ 00-Python Object and Data Structure Basics/practice.py
@@ -0,0 +1,17 @@
+# write some arithmetic that equals 100
+print(50+50)
+
+# create some variables and do math with them
+a = 5
+b = 10
+print(a+b)
+
+my_income = 100
+
+my_tax_rate = 0.1
+
+taxes_due = my_income * my_tax_rate
+
+print(type(taxes_due))
+
+print(taxes_due)
